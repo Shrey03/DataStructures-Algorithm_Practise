0085baca607f3e0cb493098fce48bd5d71b53fe3
Shrey03 <pshrey619@gmail.com>
2021-12-02 23:19:30 -0500

    DAY 3 DSA Practise

--- recursion.py
+++ recursion.py
@@ -35,8 +35,22 @@ def recursiveMethod(n):
         recursiveMethod(n-1)
         print(n)
 
-recursiveMethod(5)
+# recursiveMethod(5)
 
 
 # ############################################################
 # ############################################################
+# Factorial of a number is the product of all the integers from 1 to that number. For example, the factorial of 6 (denoted as 6!) 6*5*4*3*2*1
+
+def factorial(x):
+    """This is a recursive function
+    to find the factorial of an integer"""
+
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+
+
+num = 3
+print("The factorial of", num, "is", factorial(num))
