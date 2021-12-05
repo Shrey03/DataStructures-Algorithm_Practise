464c4aa248a66d6fff262a1aff0d0d93568fdec8
Shrey03 <pshrey619@gmail.com>
2021-12-05 16:13:20 -0500

    DAY 4 DSA Practise

--- recursion.py
+++ recursion.py
@@ -45,12 +45,37 @@ def recursiveMethod(n):
 def factorial(x):
     """This is a recursive function
     to find the factorial of an integer"""
-
-    if x == 1:
+    #############################
+    """Setting assert rule , so that thet conditions are set and it will not use all the stack memory, before using assert , 
+    recrusion method will accept non onteger numbers and negative number
+    and it will give error """
+    assert x >= 0 and int(x) == x, 'The number must be positive integers only'
+    if x == 1: 
         return 1
     else:
         return (x * factorial(x-1))
 
 
-num = 3
-print("The factorial of", num, "is", factorial(num))
+num = 4
+final =  factorial(num)
+# print("The factorial of", num, "is", final )
+
+
+
+# ############################################################
+# ############################################################
+# write a recursive fu nction to find fibonacci series 
+# 0 , 1 , 1 , 2,3,5,8,13,21
+# f(n) = f(n-1) + f(n-2)
+
+def fibonacci(n):
+    assert x >= 0 and int(x) == x, 'The number must be positive integers only'
+    if n in [0,1]:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+n = 5
+print(fibonacci(6))
+
+
